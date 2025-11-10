15fcea84d5f66840485f12bccfa4a24da7eaebce
Manjeet Kumar <manjeetgodara001@gmail.com>
2025-11-10 13:31:24 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,10 +10,23 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
-ap.use(express.json({limit: "16kb"}));
+app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
-app.use(cookieParser())
+app.use(cookieParser());
+
+
+// app.get("/",(req,res)=>{
+//     res.send("hello world");
+// }   )
+
+
+// import routes
+import userRoutes from "./routes/user.routes.js"
+
+
+// route declaration 
+app.use("/api/v1/users", userRoutes)
 
 
 export {app}
---
+++ src/routes/user.routes.js
@@ -0,0 +1,11 @@
+
+import { Router } from "express"
+import { registeUser } from "../controllers/user.controller.js"
+
+
+const router = Router()
+
+router.route("/register").post(registeUser)
+
+
+export default router
